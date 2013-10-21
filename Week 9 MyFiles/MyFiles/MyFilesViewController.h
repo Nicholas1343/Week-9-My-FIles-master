c0ffe7056461be5458bd0c6451381da0965fcf18
Nicholas1343 <NicholasSharp@Nicholass-MacBook-Pro.local>
2013-10-21 17:40:15 -0500

    Change

---
+++ Week 9 MyFiles/MyFiles/MyFilesViewController.h
@@ -0,0 +1,17 @@
+//
+//  MyFilesViewController.h
+//  MyFiles
+//
+//  Created by Nicholas Sharp on 3/25/13.
+//  Copyright (c) 2013 RVC Student. All rights reserved.
+//  Updated October 2013 
+
+#import <UIKit/UIKit.h>
+
+@interface MyFilesViewController : UIViewController
+@property (retain, nonatomic) IBOutlet UIView *WebView;
+- (IBAction)btnLocal:(id)sender;
+- (IBAction)btnWeb:(id)sender;
+
+
+@end
